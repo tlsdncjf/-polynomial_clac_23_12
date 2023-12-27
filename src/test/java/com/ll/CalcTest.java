776381161d16ec365a68aba41389d444af8c8a9c
shinwc147369@gmail.com <shinwc147369@gmail.com>
2023-12-27 09:43:59 +0900

    4단계, 빼기 기능 구현

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -8,28 +8,37 @@ public class CalcTest {
   void t1() {
     assertThat(Calc.run("1 + 1")).isEqualTo(2);
   }
-
   @Test
   @DisplayName("2 + 1 == 3")
   void t2() {
     assertThat(Calc.run("2 + 1")).isEqualTo(3);
   }
-
   @Test
   @DisplayName("2 + 2 == 4")
   void t3() {
     assertThat(Calc.run("2 + 2")).isEqualTo(4);
   }
-
   @Test
   @DisplayName("123 + 456 == 579")
   void t4() {
     assertThat(Calc.run("123 + 456")).isEqualTo(579);
   }
-
   @Test
   @DisplayName("1000 + 200 == 1200")
   void t5() {
     assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
   }
+
+  @Test
+  @DisplayName("50 - 30 == 20")
+  void t6() {
+    assertThat(Calc.run("50 - 30")).isEqualTo(20);
+  }
+
+  @Test
+  @DisplayName("3 - 1 == 2")
+  void t7() {
+    assertThat(Calc.run("3 - 1")).isEqualTo(2);
+  }
+
 }
