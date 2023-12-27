e893f41fff30a7cc34f7c1ef1788b11c585064c6
shinwc147369@gmail.com <shinwc147369@gmail.com>
2023-12-27 10:47:45 +0900

    6단계, 10 - 20 + 30

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -4,5 +4,8 @@ package com.ll;
 public class Main {
   public static void main(String[] args) {
 
+    System.out.println("10 - 20 + 30");
+    System.out.println("10 - 20 + 30".replaceAll("- ", "+ -"));
+
   }
 }
