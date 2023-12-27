bd86525556f874e6a2023b550da66dbf12456d30
shinwc147369@gmail.com <shinwc147369@gmail.com>
2023-12-27 11:20:09 +0900

    7단계, 10 - 10 - 10 - 10 + 10 + 10 - 10

--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -8,31 +8,37 @@ public class CalcTest {
   void t1() {
     assertThat(Calc.run("1 + 1")).isEqualTo(2);
   }
+
   @Test
   @DisplayName("2 + 1 == 3")
   void t2() {
     assertThat(Calc.run("2 + 1")).isEqualTo(3);
   }
+
   @Test
   @DisplayName("2 + 2 == 4")
   void t3() {
     assertThat(Calc.run("2 + 2")).isEqualTo(4);
   }
+
   @Test
   @DisplayName("123 + 456 == 579")
   void t4() {
     assertThat(Calc.run("123 + 456")).isEqualTo(579);
   }
+
   @Test
   @DisplayName("1000 + 200 == 1200")
   void t5() {
     assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
   }
+
   @Test
   @DisplayName("50 - 30 == 20")
   void t6() {
     assertThat(Calc.run("50 - 30")).isEqualTo(20);
   }
+
   @Test
   @DisplayName("3 - 1 == 2")
   void t7() {
@@ -44,9 +50,22 @@ public class CalcTest {
   void t8() {
     assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
   }
+
   @Test
   @DisplayName("10 - 20 + 30 == 20")
   void t9() {
     assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+  }
+
+  @Test
+  @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 == -10")
+  void t10() {
+    assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+  }
+
+  @Test
+  @DisplayName("10 - 10 - 10 - 10 == -20")
+  void t11() {
+    assertThat(Calc.run("10 - 10 - 10 - 10")).isEqualTo(-20);
   }
 }
