e82dea5eba3a94121c147770488cd61ba7cd858e
shinwc147369@gmail.com <shinwc147369@gmail.com>
2023-12-27 09:46:55 +0900

    5단계, 10 + 20 + 30

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -1,12 +1,9 @@
 package com.ll;
 public class Calc {
   public static int run(String exp) {
-
     boolean needToPlus = exp.contains("+");
     boolean needToMinus = exp.contains("-");
-
     String[] bits = null;
-
     if (needToPlus) {
       bits = exp.split(" \\+ ");
     } else if (needToMinus) {
@@ -15,11 +12,16 @@ public class Calc {
 
     int a = Integer.parseInt(bits[0]);
     int b = Integer.parseInt(bits[1]);
+    int c = 0;
+
+    if (bits.length > 2) {
+      c = Integer.parseInt(bits[2]);
+    }
 
     if (needToPlus) {
-      return a + b;
+      return a + b + c;
     } else if (needToMinus) {
-      return a - b;
+      return a - b - c;
     }
 
     throw new RuntimeException("처리할 수 있는 계산식이 아닙니다");
--- src/test/java/com/ll/CalcTest.java
+++ src/test/java/com/ll/CalcTest.java
@@ -28,17 +28,20 @@ public class CalcTest {
   void t5() {
     assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
   }
-
   @Test
   @DisplayName("50 - 30 == 20")
   void t6() {
     assertThat(Calc.run("50 - 30")).isEqualTo(20);
   }
-
   @Test
   @DisplayName("3 - 1 == 2")
   void t7() {
     assertThat(Calc.run("3 - 1")).isEqualTo(2);
   }
 
+  @Test
+  @DisplayName("10 + 20 + 30 == 60")
+  void t8() {
+    assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+  }
 }
